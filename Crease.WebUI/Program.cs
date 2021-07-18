63da83da55c1d4eb8fd7f29e4ca5e6608e587f54
Lai, Sze Chuan <szechuan.lai@illumina.com>
2021-07-18 14:34:42 +0800

    Update Crease solution structure

--- Crease.WebUI/Program.cs
+++ Crease.WebUI/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Crease.API
+namespace Crease.Main.WebUI
 {
     public class Program
     {
