eb0343c9a4b588f2a443d92540433126b0f7b6ed
Sze Chuan <sze.chuan@gmail.com>
2021-11-07 14:49:33 +0800

    Add dummy landing page

---
+++ src/Crease.WebUI/client-app/src/components/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Maximise your rewards</h1>
+      <span>Track your credit card rewards with ease</span>
+    </div>
+  );
+};
