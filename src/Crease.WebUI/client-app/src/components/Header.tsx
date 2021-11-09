6f6217f5c1daae709a90abb5452d8cb0deae44bd
Sze Chuan <sze.chuan@gmail.com>
2021-11-10 00:19:27 +0800

    Update header

--- src/Crease.WebUI/client-app/src/components/Header.tsx
+++ src/Crease.WebUI/client-app/src/components/Header.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 export const Header: React.FC = () => {
   return (
     <header>
       <AppBar position="static">
         <Toolbar>
-          <span>Crease</span>
+          <CreditCardIcon />
+          <Box sx={{ flexGrow: 1 }} />
+          <Button className="login-btn" variant="contained">
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </header>
