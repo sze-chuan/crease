426065053547bbff8ed61f2e463a11e8ce9eab60
Sze Chuan, Lai <szechuan.lai@illumina.com>
2023-02-21 10:21:45 +0800

    Move components to new project

--- src/Crease.Api/client-app/src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers, Reducer } from '@reduxjs/toolkit';
-import { RootState } from '../types';
-import cardReducer from './card';
-
-const rootReducer: Reducer<RootState> = combineReducers<RootState>({
-  cardState: cardReducer,
-});
-
-export default rootReducer;
