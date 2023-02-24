417bba639b1a0a62134a221cb0bcc4bcd49f918a
Sze Chuan <sze.chuan@gmail.com>
2023-02-24 15:46:24 +0800

    Remove unused divs

--- src/Crease.Client/src/components/AddTransactionDialog/index.tsx
+++ src/Crease.Client/src/components/AddTransactionDialog/index.tsx
@@ -112,82 +112,76 @@ const AddTransactionDialog = (): JSX.Element => {
         noValidate={true}
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div>
-          <div>
-            <CardImage cardName={transactionDialog.card?.name} />
-            <S.StyledFormControl fullWidth>
-              <Controller
-                name="transactionDate"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <DatePicker
-                    {...field}
-                    label="Transaction Date"
-                    inputFormat="dd/MM/yyyy"
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        required
-                        error={errors.transactionDate ? true : false}
-                        helperText={errors.transactionDate?.message ?? ''}
-                      />
-                    )}
+        <CardImage cardName={transactionDialog.card?.name} />
+        <S.StyledFormControl fullWidth>
+          <Controller
+            name="transactionDate"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <DatePicker
+                {...field}
+                label="Transaction Date"
+                inputFormat="dd/MM/yyyy"
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    required
+                    error={errors.transactionDate ? true : false}
+                    helperText={errors.transactionDate?.message ?? ''}
                   />
                 )}
               />
-            </S.StyledFormControl>
-            <S.StyledFormControl fullWidth>
-              <TextField
-                required
-                label="Amount"
-                variant="outlined"
-                {...register('amount')}
-                error={errors.amount ? true : false}
-                helperText={errors.amount?.message ?? ''}
-              />
-            </S.StyledFormControl>
-            <S.StyledFormControl fullWidth>
-              <TextField
-                required
-                label="Description"
-                variant="outlined"
-                {...register('description')}
-                error={errors.description ? true : false}
-                helperText={errors.description?.message ?? ''}
-              />
-            </S.StyledFormControl>
-            <FormSelect name="category" control={control} labelText="Category">
-              {PaymentCategories.map((category) => (
-                <MenuItem key={category} value={category.toLowerCase()}>
-                  {category}
-                </MenuItem>
-              ))}
-            </FormSelect>
-            <FormSelect
-              name="transactionType"
-              control={control}
-              labelText="Transaction type"
-            >
-              {transactionTypes.map((transactionType) => (
-                <MenuItem key={transactionType} value={transactionType}>
-                  {transactionType}
-                </MenuItem>
-              ))}
-            </FormSelect>
-          </div>
-          <div>
-            <S.StyledFormControl fullWidth>
-              <S.StyledSubmitButton
-                type="submit"
-                variant="contained"
-                disabled={Object.keys(errors).length > 0}
-              >
-                Add
-              </S.StyledSubmitButton>
-            </S.StyledFormControl>
-          </div>
-        </div>
+            )}
+          />
+        </S.StyledFormControl>
+        <S.StyledFormControl fullWidth>
+          <TextField
+            required
+            label="Amount"
+            variant="outlined"
+            {...register('amount')}
+            error={errors.amount ? true : false}
+            helperText={errors.amount?.message ?? ''}
+          />
+        </S.StyledFormControl>
+        <S.StyledFormControl fullWidth>
+          <TextField
+            required
+            label="Description"
+            variant="outlined"
+            {...register('description')}
+            error={errors.description ? true : false}
+            helperText={errors.description?.message ?? ''}
+          />
+        </S.StyledFormControl>
+        <FormSelect name="category" control={control} labelText="Category">
+          {PaymentCategories.map((category) => (
+            <MenuItem key={category} value={category.toLowerCase()}>
+              {category}
+            </MenuItem>
+          ))}
+        </FormSelect>
+        <FormSelect
+          name="transactionType"
+          control={control}
+          labelText="Transaction type"
+        >
+          {transactionTypes.map((transactionType) => (
+            <MenuItem key={transactionType} value={transactionType}>
+              {transactionType}
+            </MenuItem>
+          ))}
+        </FormSelect>
+        <S.StyledFormControl fullWidth>
+          <S.StyledSubmitButton
+            type="submit"
+            variant="contained"
+            disabled={Object.keys(errors).length > 0}
+          >
+            Add
+          </S.StyledSubmitButton>
+        </S.StyledFormControl>
       </form>
     </DialogTemplate>
   );
