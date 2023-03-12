7727f9824a2a9375e26804a62207dd2395592cbe
Sze Chuan <sze.chuan@gmail.com>
2023-03-12 22:56:04 +0800

    Add constants

---
+++ src/Crease.Api/Constants.cs
@@ -0,0 +1,16 @@
+namespace Crease.WebUI;
+
+public class Constants
+{
+    public class Scopes
+    {
+        public const string BankCardRead = "bankcard.read";
+        public const string CardRead = "card.read";
+        public const string CardWrite = "card.write";
+        public const string CardStatementRead = "cardstatement.read";
+        public const string CardStatementWrite = "cardstatement.write";
+        public const string TransactionWrite = "transaction.write";
+        public const string TransactionRead = "transaction.read";
+        public const string TransactionDelete = "transaction.delete";
+    }
+}
