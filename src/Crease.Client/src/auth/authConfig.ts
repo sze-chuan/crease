ba248a423ce2343ea4b60fc2b9657357e5abd19f
Sze Chuan <sze.chuan@gmail.com>
2023-03-13 22:24:42 +0800

    Update authconfig

--- src/Crease.Client/src/auth/authConfig.ts
+++ src/Crease.Client/src/auth/authConfig.ts
@@ -47,8 +47,8 @@ export const msalConfig: Configuration = {
     clientId: '7509f265-77f0-40c6-873d-cc002da7166f',
     authority: b2cPolicies.authorities.signUpSignIn.authority,
     knownAuthorities: [b2cPolicies.authorityDomain],
-    redirectUri: '/',
-    postLogoutRedirectUri: '/',
+    redirectUri: `${process.env.REACT_APP_PUBLIC_URL}`,
+    postLogoutRedirectUri: `${process.env.REACT_APP_PUBLIC_URL}`,
   },
   cache: {
     cacheLocation: BrowserCacheLocation.LocalStorage,
@@ -68,7 +68,7 @@ export const msalConfig: Configuration = {
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 export const loginRequest: RedirectRequest = {
   scopes: apiScopes,
-  redirectStartPage: '/home',
+  redirectStartPage: `${process.env.REACT_APP_PUBLIC_URL}/home`,
 };
 
 export const tokenRequest: SilentRequest = {
